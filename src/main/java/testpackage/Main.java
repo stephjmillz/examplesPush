cd1c2326452d8d2cd95bf0916f286fbdec87f11c
Steph J Millz <stephjmillz@outlook.com>
2020-03-12 06:36:57 -0700

    Ima real niqqa

---
+++ src/main/java/testpackage/Main.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Declaring variable
+        byte x = 127;
+        System.out.println(x);
+
+        float myFloat = 3.12f;
+        System.out.println(myFloat);
+
+        
+    }
+}
