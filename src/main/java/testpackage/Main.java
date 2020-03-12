91772f6a6936ff6750cb9dc803e572e753251de9
Steph J Millz <stephjmillz@outlook.com>
2020-03-12 07:19:55 -0700

    New shxt lol

--- src/main/java/testpackage/Main.java
+++ src/main/java/testpackage/Main.java
@@ -4,13 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Declaring variable
+        /*Declaring variable
         byte x = 127;
         System.out.println(x);
 
         float myFloat = 3.12f;
         System.out.println(myFloat);
+        */
 
-        
+        for(int y = 0; y < 10; y++) {
+            System.out.println(y);
+        }
+
+            for(int p = 1; p <= 5; p++) {
+            System.out.println(p);
+
+
+
+        }
     }
 }
